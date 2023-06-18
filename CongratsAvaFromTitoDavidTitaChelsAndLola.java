932abdf7ea893825a461da535579ba6537cb8e69
guavathieves <david.macadangdang@gmail.com>
2023-06-18 01:25:35 -0700

    Update CongratsAvaFromTitoDavidTitaChelsAndLola.java

--- CongratsAvaFromTitoDavidTitaChelsAndLola.java
+++ CongratsAvaFromTitoDavidTitaChelsAndLola.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-class CongratsAvaFromTitoDavidTitaChelsAndLola {
+class FromTitoDavidTitaChelsAndLola {
     public static void main(String[] args) {
         class Headspace {
             private final String mood;
